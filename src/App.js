9cd849ac561d2ae9368826576b34274862777e98
pradyunrao <riffreefer@gmail.com>
2022-08-28 15:36:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <div className=" item item_1">
+          <h1>PencilHacker Test</h1>
+        </div>
+        <div className="item item_2">
+          <h2>Piet Mondrian</h2>
+          <p>Pieter Cornelis Mondriaan was a Dutch painter and art theoretician who is regarded as one of the greatest artists of the 20th century. He is known for being one of the pioneers of 20th-century abstract art, as he changed his artistic direction from figurative painting to an increasingly abstract style, until he reached a point where his artistic vocabulary was reduced to simple geometric elements.</p>
+          <p>Mondrian's art was highly utopian and was concerned with a search for universal values and aesthetics. He proclaimed in 1914: "Art is higher than reality and has no direct relation to reality. To approach the spiritual in art, one will make as little use as possible of reality, because reality is opposed to the spiritual. We find ourselves in the presence of an abstract art. Art should be above reality, otherwise it would have no value for man." His art, however, always remained rooted in nature.</p>
+        </div>
+        <div className="item item_3">Sidebar_1</div>
+        <div className="item item_4">Sidebar_2</div>
+        <div className="item item_5">Footer</div>
+        <div className="item item_6">2nd Section</div>
+      </div>
     </div>
   );
 }
