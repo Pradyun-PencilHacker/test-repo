0f14c383cc8f38c07bee485829177e84c5b4d5d6
pradyunrao <pradyun@pencilhacker.com>
2022-09-01 17:35:53 +0530

    Load more pagination added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [visible, setVisible] = useState(6);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  const showMoreItems = () => {
+    setVisible((prevState) => prevState + 3);
+  };
+
   return (
     <div>
       <div className="container">
-        <div className=" item item_1">
-          <h1>PencilHacker Test</h1>
-        </div>
-        <div className="item item_2">
-          <h2>Piet Mondrian</h2>
-          <p>Pieter Cornelis Mondriaan was a Dutch painter and art theoretician who is regarded as one of the greatest artists of the 20th century. He is known for being one of the pioneers of 20th-century abstract art, as he changed his artistic direction from figurative painting to an increasingly abstract style, until he reached a point where his artistic vocabulary was reduced to simple geometric elements.</p>
-          <p>Mondrian's art was highly utopian and was concerned with a search for universal values and aesthetics. He proclaimed in 1914: "Art is higher than reality and has no direct relation to reality. To approach the spiritual in art, one will make as little use as possible of reality, because reality is opposed to the spiritual. We find ourselves in the presence of an abstract art. Art should be above reality, otherwise it would have no value for man." His art, however, always remained rooted in nature.</p>
-        </div>
-        <div className="item item_3">Sidebar_1</div>
-        <div className="item item_4">Sidebar_2</div>
-        <div className="item item_5">Footer</div>
-        <div className="item item_6">2nd Section</div>
+        {items.slice(0, visible).map((item) => (
+          <div className="card">
+            <img src="https://source.unsplash.com/random/600x300?v=1"></img>
+            <div>
+              <p>{item.title}</p>
+            </div>
+          </div>
+        ))}
+         <button onClick={showMoreItems}>Load More</button>
       </div>
+     
     </div>
   );
 }
